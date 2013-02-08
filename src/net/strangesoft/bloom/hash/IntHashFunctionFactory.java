f1afe985997f683fd7fffa0e4ddda72a1c99997a
Igor Bezugliy <brenagwynn@gmail.com>
2013-02-08 15:48:11 +0200

    Added licensing information into sources

--- src/net/strangesoft/bloom/hash/IntHashFunctionFactory.java
+++ src/net/strangesoft/bloom/hash/IntHashFunctionFactory.java
@@ -1,5 +1,22 @@
 package net.strangesoft.bloom.hash;
 
+/*
+Copyright (C) 2013 Igor Bezugliy
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
 This abstract class must be extended and getHashFunction method must be implemented so it supplies IntHashFunction instances according to the index.
 @see net.strangesoft.bloom.hash.murmur2.Murmur2Factory
